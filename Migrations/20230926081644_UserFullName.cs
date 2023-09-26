429d2cb12e6c7c3c2dbdb744754f6b519ceb8b46
Ignacy Starczynski <ignacysan27@gmail.com>
2023-09-26 10:31:26 +0200

    Custom migration

---
+++ Migrations/20230926081644_UserFullName.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Board.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserFullName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.Sql(
+            @"Update users Set FullName = FirstName + ' ' + LastName");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            //Update new columns
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "users");
+        }
+    }
+}
