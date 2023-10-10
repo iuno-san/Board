03d3f6b388b32ad92593eefe65f8ffa0c6daee6f
Ignacy Starczynski <ignacysan27@gmail.com>
2023-10-10 07:12:13 +0200

    Seedowanie(manually customizing migrations)

---
+++ Migrations/20231010045206_AddWorkitemstateSeed.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Board.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWorkitemstateSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "workItemStates",
+                column: "Value",
+                value: "Rejected" );
+
+            migrationBuilder.InsertData(
+                table: "workItemStates",
+                column: "Value",
+                value: "On Hold");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "workItemStates",
+                keyColumn: "Value",
+                keyValue: "Rejected" );
+
+            migrationBuilder.DeleteData(
+               table: "workItemStates",
+               keyColumn: "Value",
+               keyValue: "On Hold");
+        }
+    }
+}
