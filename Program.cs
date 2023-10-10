58b06fd5729261d5c551d99a1b5ff7eb0a0e7252
Ignacy Starczynski <ignacysan27@gmail.com>
2023-10-10 08:16:36 +0200

    Seedowanie(custom data seeding)

--- Program.cs
+++ Program.cs
@@ -30,6 +30,29 @@ if (pendingMigrations.Any())
     dbContext.Database.Migrate();
 }
 
+var users = dbContext.users.ToList();
+if (!users.Any())
+{
+    var user1 = new User()
+    {
+        FullName = "userOne",
+        Email = "userOne@test.com",
+        Address = new Address() { City = "London", Street = "Newgate 12" }
+        
+    };
+    var user2 = new User()
+    {
+        FullName = "userTwo",
+        Email = "userTwo@test.com",
+        Address = new Address() { City = "Dortmund", Street = "Hagener 10" }
+
+    };
+
+    dbContext.users.AddRange(user1, user2);
+
+    dbContext.SaveChanges();
+}
+
 
 app.Run();
 
