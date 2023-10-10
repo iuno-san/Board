6cd43a9dd67131769bb00d1c050fc19824db57ce
Ignacy Starczynski <ignacysan27@gmail.com>
2023-10-10 08:25:54 +0200

    Tag entity seeding
    add value:
    - Web
    - UI
    - Desktop
    - API
    - Service

---
+++ Migrations/20231010062044_TagSeed.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Board.Migrations
+{
+    /// <inheritdoc />
+    public partial class TagSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "tags",
+                columns: new[] { "Id", "Value" },
+                values: new object[,]
+                {
+                    { 1, "Web" },
+                    { 2, "UI" },
+                    { 3, "Desktop" },
+                    { 4, "API" },
+                    { 5, "Service" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "tags",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "tags",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "tags",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "tags",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "tags",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
